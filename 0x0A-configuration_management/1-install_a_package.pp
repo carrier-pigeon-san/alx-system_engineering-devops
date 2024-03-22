608097d572a99861502a2ca68aad41c75b5111ad
Sandy Obare <obaresandy@gmail.com>
2024-03-22 20:47:12 +0000

    1-install_a_package.pp: additional package resource to install flask package dependency

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Manifest installs flask from pip3
 # Package version: 2.1.0
 
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
