9dab939fcf6c0bd43f4961339ab944e87cb5eebf
Sandy Obare <obaresandy@gmail.com>
2024-06-28 23:23:18 +0300

    0-create_a_file.pp initial commit This manifest creates the file /tmp/school: > * File permission - 0744 > * File owner - www-data > * File group - www-data > * File content - 'I love Puppet'

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,13 @@
+# A puppet manifest that creates a file '/tmp/school'
+# File permission is 0744
+# File owner is www-data
+# File group is www-data
+# File contains I love Puppet'
+
+file { '/tmp/school'
+  ensure => 'present',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
+}
