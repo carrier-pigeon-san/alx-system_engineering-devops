3b1a7264a9b9ce9ef69bbe9e6602fc046dd9be8c
Sandy Obare <obaresandy@gmail.com>
2024-06-28 23:26:28 +0300

    0-create_a_file.pp manifest update Formatting updates

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,10 +4,10 @@
 # File group is www-data
 # File contains I love Puppet'
 
-file { '/tmp/school'
-  ensure => 'present',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
