58bd887fbadde921ca6892d13c543bf042af477d
Sandy Obare <obaresandy@gmail.com>
2024-07-30 00:27:01 +0300

    0-gather_data_from_an_API.py update #2 Import packages in alphabetical order Output format adjustment

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,12 +2,11 @@
 """This module contains a python script that return information about a given
 user's TODO list progress from a REST API endpoint
 """
-from sys import argv
-from requests import exceptions
 import requests
+import sys
 
 if __name__ == '__main__':
-    user_id = argv[1]
+    user_id = sys.argv[1]
     user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     todos_url = f'{user_url}/todos'
 
@@ -17,11 +16,11 @@ if __name__ == '__main__':
     try:
         user = u.json()
         todos = t.json()
-    except exceptions.JSONDecodeError:
+    except requests.exceptions.JSONDecodeError:
         pass
     else:
         done = [i for i in todos if i.get('completed') is True]
-        print('Employee {} is done with tasks ({}/{}):'.format(
+        print('Employee {} is done with tasks({}/{}):'.format(
             user.get('name'), len(done), len(todos)))
         for i in done:
             print(f"\t {i.get('title')}")
