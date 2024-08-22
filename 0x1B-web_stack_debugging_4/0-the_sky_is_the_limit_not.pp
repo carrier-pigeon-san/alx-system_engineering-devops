e05453a01d1c67040ec5830412995833ce37b160
Sandy Obare <obaresandy@gmail.com>
2024-08-23 01:34:38 +0300

    0-the_sky_is_the_limit_not.pp update: update to comment

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# A puppet file that changes a line in wp-settings.php file
+# This manifest resets ULIMIT directive in /etc/default/nginx file
 
 exec { 'sed -i "s/15/5000/g" /etc/default/nginx':
   path => 'usr/bin:/usr/sbin:/bin',
